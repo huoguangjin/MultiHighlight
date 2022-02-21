1cc7fba98a4b8388446926c169f1723566bd7279
huoguangjin <352886006@qq.com>
2022-02-21 23:33:03 +0800

    refactor: rewrite NamedTextAttr in kotlin

---
+++ src/main/java/com/github/huoguangjin/multihighlight/config/NamedTextAttr.kt
@@ -0,0 +1,49 @@
+package com.github.huoguangjin.multihighlight.config
+
+import com.intellij.openapi.editor.colors.EditorColors
+import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.editor.markup.TextAttributes
+import com.intellij.util.castSafelyTo
+import org.jdom.Element
+
+class NamedTextAttr : TextAttributes {
+
+  var name: String
+
+  constructor(name: String, ta: TextAttributes) : super() {
+    this.name = name
+    copyFrom(ta)
+  }
+
+  constructor(element: Element) : super(element) {
+    val attribute = element.getAttribute(ATTR_NAME)
+    name = attribute?.value ?: ""
+  }
+
+  override fun writeExternal(element: Element) {
+    super.writeExternal(element)
+    element.setAttribute(ATTR_NAME, name)
+  }
+
+  override fun clone(): NamedTextAttr = NamedTextAttr(name, this)
+
+  override fun equals(other: Any?): Boolean {
+    val o = other.castSafelyTo<NamedTextAttr>() ?: return false
+    return name == o.name && super.equals(o)
+  }
+
+  override fun hashCode(): Int = 31 * name.hashCode() + super.hashCode()
+
+  override fun toString(): String = "NamedTextAttr{" + name + "=" + super.toString() + '}'
+
+  companion object {
+
+    private const val ATTR_NAME = "name"
+
+    @JvmField
+    val IDE_DEFAULT = NamedTextAttr(
+      "IDE default",
+      EditorColorsManager.getInstance().globalScheme.getAttributes(EditorColors.SEARCH_RESULT_ATTRIBUTES)
+    )
+  }
+}
