e054d6c0aa4b50e7dcd9618d79550a013f616fb8
huoguangjin <352886006@qq.com>
2022-04-21 07:07:34 +0800

    feat: highlight text ranges in injected code fragment

--- src/main/java/com/github/huoguangjin/multihighlight/highlight/MultiHighlightTextHandler.kt
+++ src/main/java/com/github/huoguangjin/multihighlight/highlight/MultiHighlightTextHandler.kt
@@ -3,12 +3,13 @@ package com.github.huoguangjin.multihighlight.highlight
 import com.github.huoguangjin.multihighlight.config.TextAttributesFactory
 import com.intellij.find.FindManager
 import com.intellij.find.FindModel
+import com.intellij.injected.editor.EditorWindow
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
-import com.intellij.refactoring.suggested.range
+import com.intellij.psi.impl.source.tree.injected.InjectedLanguageEditorUtil
 
 class MultiHighlightTextHandler(
   private val project: Project,
@@ -31,6 +32,9 @@ class MultiHighlightTextHandler(
   }
 
   fun tryRemoveHighlighters(): Boolean {
+    // TODO: 2022/4/21 move to MultiHighlightManager
+    val editor = InjectedLanguageEditorUtil.getTopLevelEditor(editor)
+
     val multiHighlightManager = MultiHighlightManager.getInstance()
     val highlighter = multiHighlightManager.findHighlightAtCaret(editor) ?: return false
 
@@ -46,7 +50,20 @@ class MultiHighlightTextHandler(
     val multiHighlightManager = MultiHighlightManager.getInstance()
     val textRanges = findText(text)
     val textAttr = TextAttributesFactory.getNextTextAttr()
-    multiHighlightManager.addHighlighters(editor, textAttr, textRanges)
+
+    if (editor is EditorWindow) {
+      // The text ranges are found in the injected editor which is an EditorWindow. Different EditorWindows created from
+      // the same injected document (DocumentWindow) are not referenced in the EditorFactory, but their host editors can
+      // be referenced by the same host document. Highlight the text ranges in the host editors so that we can also
+      // add/remove highlighters across editors.
+      // see: [com.intellij.openapi.editor.impl.EditorFactoryImpl.createEditor]
+      val hostEditor = editor.delegate
+      val injectedDocument = editor.document
+      val hostTextRanges = textRanges.map(injectedDocument::injectedToHost)
+      multiHighlightManager.addHighlighters(hostEditor, textAttr, hostTextRanges)
+    } else {
+      multiHighlightManager.addHighlighters(editor, textAttr, textRanges)
+    }
   }
 
   private fun findText(text: String): MutableList<TextRange> {
