be7ec02c9ae8fe807dfb25f31212c90e9179f450
huoguangjin <352886006@qq.com>
2023-05-04 12:26:15 +0800

    build: update gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.7.0"
-    id("org.jetbrains.intellij") version "1.6.0"
-    id("org.jetbrains.changelog") version "1.3.1"
+    id("org.jetbrains.intellij") version "1.13.3"
+    id("org.jetbrains.changelog") version "2.0.0"
     id("org.jetbrains.qodana") version "0.1.13"
 }
 
