798637396e866f74ddab7e59c1cbcdffa5bdd307
huoguangjin <352886006@qq.com>
2023-05-05 11:09:06 +0800

    build: fix changelog config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -80,14 +81,19 @@ tasks {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
                 }
                 subList(indexOf(start) + 1, indexOf(end))
-            }.joinToString("\n").run { markdownToHTML(this) }
+            }.joinToString("\n").let(::markdownToHTML)
         )
 
+        val changelog = project.changelog // local variable for configuration cache compatibility
         // Get the latest available change notes from the changelog file
         changeNotes.set(provider {
             changelog.run {
-                getOrNull(pluginVersion) ?: getLatest()
-            }.toHTML()
+                val changeLogItem = getOrNull(pluginVersion) ?: getUnreleased()
+                renderItem(
+                    changeLogItem.withHeader(false).withEmptySections(false),
+                    Changelog.OutputType.HTML,
+                )
+            }
         })
     }
 
