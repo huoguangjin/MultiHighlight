53ac148b33a57ca1e822889967d702b950c9b1b0
huoguangjin <352886006@qq.com>
2024-06-23 11:22:22 +0800

    feat: simplify code

--- src/main/java/com/github/huoguangjin/multihighlight/highlight/HighlightUsagesHelper.java
+++ src/main/java/com/github/huoguangjin/multihighlight/highlight/HighlightUsagesHelper.java
@@ -4,26 +4,23 @@ import com.intellij.codeInsight.highlighting.HighlightUsagesKt;
 import com.intellij.model.Symbol;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiFile;
+import kotlin.Pair;
+import kotlin.collections.CollectionsKt;
 
 import java.util.ArrayList;
-
-import kotlin.Pair;
+import java.util.List;
 
 class HighlightUsagesHelper {
 
-  @SuppressWarnings({ "UnstableApiUsage", "KotlinInternalInJava" })
-  public static Pair<ArrayList<TextRange>, ArrayList<TextRange>> getUsageRanges(PsiFile file, Symbol symbol) {
-    ArrayList<TextRange> readRanges = new ArrayList<>();
-    ArrayList<TextRange> writeRanges = new ArrayList<>();
+    @SuppressWarnings({"UnstableApiUsage", "KotlinInternalInJava"})
+    public static Pair<List<TextRange>, List<TextRange>> getUsageRanges(PsiFile file, Symbol symbol) {
+        var usageRanges = HighlightUsagesKt.getUsageRanges(file, symbol);
+        if (usageRanges == null) {
+            return new Pair<>(new ArrayList<>(), new ArrayList<>());
+        }
 
-    var usageRanges = HighlightUsagesKt.getUsageRanges(file, symbol);
-    if (usageRanges != null) {
-      readRanges.addAll(usageRanges.getReadRanges());
-      readRanges.addAll(usageRanges.getReadDeclarationRanges());
-      writeRanges.addAll(usageRanges.getWriteRanges());
-      writeRanges.addAll(usageRanges.getWriteDeclarationRanges());
+        var allReadRanges = CollectionsKt.plus(usageRanges.getReadRanges(), usageRanges.getReadDeclarationRanges());
+        var allWriteRanges = CollectionsKt.plus(usageRanges.getWriteRanges(), usageRanges.getWriteDeclarationRanges());
+        return new Pair<>(allReadRanges, allWriteRanges);
     }
-
-    return new Pair<>(readRanges, writeRanges);
-  }
 }
